2cc7a672b91cab1b841a5d2b799963a3919b33da
Azyte <darurahmatanggoro@gmail.com>
2026-07-26 16:03:52 +0700

    fix: input clickable cursor

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -7,7 +7,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => (
     <input
       type={type}
-      className={cn("input-retro flex h-11 w-full rounded-xl px-4 text-sm outline-none disabled:opacity-50", className)}
+      className={cn(
+        "input-retro flex h-11 w-full rounded-xl px-4 text-sm outline-none disabled:opacity-50 cursor-text",
+        className
+      )}
       ref={ref}
       {...props}
     />
